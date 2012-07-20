d144995066d566182b618c17aa335c7207e54595
Stian Liknes <stianlik@gmail.com>
2012-07-21 00:13:16 +0200

    Refactored test

--- tests/ERestControllerTest.php
+++ tests/ERestControllerTest.php
@@ -1,39 +1,50 @@
 <?php
 class ERestControllerTest extends CDbTestCase {
 
-	public function testShould_be_able_to_create_new_user() {
+	protected function mockRequest($request) {
+		$requestJson = json_encode($request);
+		$mock = $this->getMock('ERequestReader', array('getContents'));
+		$mock->expects($this->once())
+			->method('getContents')
+			->will($this->returnValue($requestJson)); 
+		return $mock;
+	}
+
+	public function mockModel($attributes) {
+		$callback = new HasAttributeCallack($attributes);
+		$mock = $this->getMock('stdClass', array('save', 'hasAttribute'));
+		$mock->expects($this->any())->method('hasAttribute')
+			->will($this->returnCallback(array($callback, 'hasAttribute')));
+		return $mock;
+	}
+
+	public function getController($request, $model) {
+		$controller = new ERestController('User');
+		$controller->requestReader = $request;
+		$controller->model = $model;
+		return $controller;
+	}
+
+	public function testShould_be_able_to_create_new_entry() {
 
 		// Request
 
-		$requestJson = json_encode(array(
+		$requestMock = $this->mockRequest(array(
 			'username' => 'support@foogile.com',
 			'password' => 'hardpassword'
 		));
 
-		$requestReaderMock = $this->getMock('ERequestReader', array('getContents'));
-		$requestReaderMock->expects($this->once())
-			->method('getContents')
-			->will($this->returnValue($requestJson)); 
-
 		// Model
 
-		$modelCallback = new HasAttributeCallack(array('username', 'password'));
-		$modelMock = $this->getMock('stdClass', array('save', 'hasAttribute'));
+		$modelMock = $this->mockModel(array('username', 'password'));
 		$modelMock->expects($this->once())->method('save')->will($this->returnValue(true));
-		$modelMock->expects($this->any())->method('hasAttribute')->will($this->returnCallback(array($modelCallback, 'hasAttribute')));
 		$modelMock->id = 2;
-
-		// Controller
-
-		$controller = new ERestController('User');
-		$controller->requestReader = $requestReaderMock;
-		$controller->model = $modelMock;
 
 		// Make request
 
 		ob_start();
 
-		$controller->actionRestCreate();
+		$this->getController($requestMock, $modelMock)->actionRestCreate();
 		$result = json_decode(ob_get_contents());
 		$this->assertEquals($result->success, true);
 
