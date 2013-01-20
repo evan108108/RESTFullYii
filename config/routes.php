417ba7e5efb7f6a8f9ffa80f1a405000f8c1d344
Evan Frohlich <evan.frohlich@controlgroup.com>
2013-01-19 20:27:28 -0500

    updated routes

--- config/routes.php
+++ config/routes.php
@@ -5,9 +5,9 @@
         'api/<controller:\w+>/<id:\w+>/<var:\w*>'=>array('<controller>/restView', 'verb'=>'GET'),
         'api/<controller:\w+>/<id:\w+>/<var:\w*>/<var2:\w*>'=>array('<controller>/restView', 'verb'=>'GET'),
         
-        array('<controller>/restUpdate', 'pattern'=>'api/<controller:\w+>/<id:\d+>', 'verb'=>'PUT'),
-        array('<controller>/restUpdate', 'pattern'=>'api/<controller:\w+>/<var:\w+>/<id:\d+>', 'verb'=>'PUT'),
-        array('<controller>/restDelete', 'pattern'=>'api/<controller:\w+>/<id:\d+>', 'verb'=>'DELETE'),
+        array('<controller>/restUpdate', 'pattern'=>'api/<controller:\w+>/<id:\w+>', 'verb'=>'PUT'),
+        array('<controller>/restUpdate', 'pattern'=>'api/<controller:\w+>/<var:\w+>/<id:\w+>', 'verb'=>'PUT'),
+        array('<controller>/restDelete', 'pattern'=>'api/<controller:\w+>/<id:\w+>', 'verb'=>'DELETE'),
         array('<controller>/restCreate', 'pattern'=>'api/<controller:\w+>', 'verb'=>'POST'),
         array('<controller>/restCreate', 'pattern'=>'api/<controller:\w+>/<id:\w+>', 'verb'=>'POST'),
         
