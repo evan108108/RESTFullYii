1b6acc6cabcad045181eee907b82e1881898cc02
drlev <drlev52@gmail.com>
2013-02-14 10:34:18 +0200

    add filter operators4

--- components/ERestHelperScopes.php
+++ components/ERestHelperScopes.php
@@ -1,106 +1,138 @@
 <?php
-class ERestHelperScopes extends CActiveRecordBehavior
-{
 
-  public function limit($limit)
-  {
-    $this->Owner->getDbCriteria()->mergeWith(array(
-      'limit'=>$limit
-    ));
-    return $this->Owner;
-  }
+class ERestHelperScopes extends CActiveRecordBehavior {
 
-  public function offset($offset)
-    {
-    $this->Owner->getDbCriteria()->mergeWith(array(
-      'offset'=>$offset
-    ));
-    return $this->Owner;
-  }
-
-  public function orderBy($field, $dir='ASC')
-  {
-    if(empty($field)) return $this->Owner;
-
-    if(!is_array($orderListItems = CJSON::decode($field)))
-    {
-       $this->Owner->getDbCriteria()->mergeWith(array(
-        'order'=>$this->getSortSQL($field, $dir)
-      ));
-      return $this->Owner;
+    public function limit($limit) {
+        $this->Owner->getDbCriteria()->mergeWith(array(
+            'limit' => $limit
+        ));
+        return $this->Owner;
     }
-    else
-    {
-      $orderByStr = "";
-      foreach($orderListItems as $orderListItem)
-        $orderByStr .= ((!empty($orderByStr))? ", " : "") . $this->getSortSQL($orderListItem['property'], $orderListItem['direction']);
-      
-      $this->Owner->getDbCriteria()->mergeWith(array(
-        'order'=>$orderByStr
-      ));
-      return $this->Owner;
-    }
-  }
 
-  public function filter($filter)
-  {
-    if(empty($filter)) return $this->Owner;
-    
-    if(!is_array($filter))
-      $filterItems = CJSON::decode($filter);
-    else
-      $filterItems = $filter;
-  
-    $query = "";
-    $params = array();
-    foreach($filterItems as $filterItem)
-    {
-      if(!is_null($filterItem['property']))
-      {
-        $cType = $this->getFilterCType($filterItem['property']);
-        
-        if($cType == 'text' || $cType == 'string')
-        {
-          
-          $compare = " LIKE :" . $filterItem['property'];
-          $params[(":" . $filterItem['property'])] = '%' . $filterItem['value'] . '%';
+    public function offset($offset) {
+        $this->Owner->getDbCriteria()->mergeWith(array(
+            'offset' => $offset
+        ));
+        return $this->Owner;
+    }
+
+    public function orderBy($field, $dir = 'ASC') {
+        if (empty($field))
+            return $this->Owner;
+
+        if (!is_array($orderListItems = CJSON::decode($field))) {
+            $this->Owner->getDbCriteria()->mergeWith(array(
+                'order' => $this->getSortSQL($field, $dir)
+            ));
+            return $this->Owner;
+        } else {
+            $orderByStr = "";
+            foreach ($orderListItems as $orderListItem)
+                $orderByStr .= ((!empty($orderByStr)) ? ", " : "") . $this->getSortSQL($orderListItem['property'], $orderListItem['direction']);
+
+            $this->Owner->getDbCriteria()->mergeWith(array(
+                'order' => $orderByStr
+            ));
+            return $this->Owner;
         }
+    }
+
+    public function filter($filter) {
+        if (empty($filter))
+            return $this->Owner;
+
+        $props = array();
+
+        if (!is_array($filter))
+            $filterItems = CJSON::decode($filter);
         else
-        {
-          $compare = " = :" . $filterItem['property'];
-          $params[(":" . $filterItem['property'])] = $filterItem['value'];
+            $filterItems = $filter;
+
+        $query = "";
+        $params = array();
+        foreach ($filterItems as $filterItem) {
+            if (!is_null($filterItem['property'])) {
+                $c = 0;
+                $prop = $filterItem['property'] . $c;
+                while (in_array($prop, $props)) {
+                    $c++;
+                    $prop = $filterItem['property'] . $c;
+                }
+                $props[] = $prop;
+                $value = $filterItem['value'];
+                $field = $filterItem['property'];
+                $cType = $this->getFilterCType($field);
+
+                if (array_key_exists('operator', $filterItem) || is_array($value)) {
+                    if (is_array($value)) {
+                        $operator = 'in';
+                    } else {
+                        $operator = strtolower($filterItem['operator']);
+                    }
+                    switch ($operator) {
+                        case 'in':
+                            $paramsStr = '';
+                            foreach ($value as $index => $item) {
+                                $params[(":" . $prop . '_' . $index)] = $item;
+                            }
+                            $paramsStr = implode(',', array_keys($params));
+                            $compare = " IN ({$paramsStr})";
+                            break;
+                        case 'like':
+                            $compare = " LIKE :" . $prop;
+                            $params[(":" . $prop)] = '%' . $value . '%';
+                            break;
+                        case '=' :
+                        case '<' :
+                        case '<=':
+                        case '>' :
+                        case '>=':
+                            $compare = " $operator :" . $prop;
+                            $params[(":" . $prop)] = $value;
+                            break;
+                        default :
+                            $compare = " = :" . $prop;
+                            $params[(":" . $prop)] = $value;
+                            break;
+                    }
+                } else {
+                    if ($cType == 'text' || $cType == 'string') {
+
+                        $compare = " LIKE :" . $prop;
+                        $params[(":" . $prop)] = '%' . $value . '%';
+                    } else {
+                        $compare = " = :" . $prop;
+                        $params[(":" . $prop)] = $value;
+                    }
+                }
+
+                $query .= (empty($query) ? "(" : " AND ") . $this->getFilterAlias($field) . '.' . $field . $compare;
+            }
         }
+        if (empty($query))
+            return $this->Owner;
 
-        $query .= (empty($query)? "(": " AND ") . $this->getFilterAlias($filterItem['property']) . '.' . $filterItem['property'] . $compare;
-      }
+        $query .= ")";
+
+        $this->Owner->getDbCriteria()->mergeWith(array(
+            'condition' => $query, 'params' => $params
+        ));
+        return $this->Owner;
     }
-    if(empty($query)) return $this->Owner;
 
-    $query .= ")";
-      
+    private function getFilterCType($property) {
+        if ($this->Owner->hasAttribute($property))
+            return $this->Owner->metaData->columns[$property]->type;
 
-    $this->Owner->getDbCriteria()->mergeWith(array(
-        'condition'=>$query, 'params'=>$params
-      ));
-    return $this->Owner;
-  }
+        return 'text';
+    }
 
-  private function getFilterCType($property)
-  {
-    if($this->Owner->hasAttribute($property))
-      return $this->Owner->metaData->columns[$property]->type;
-    
-    return 'text';
-  }
+    private function getFilterAlias($property) {
+        return $this->Owner->getTableAlias(false, false);
+    }
 
-  private function getFilterAlias($property)
-  {
-    return $this->Owner->getTableAlias(false, false);
-  }
-
-  private function getSortSQL($field, $dir='ASC')
-  {
-    return $this->Owner->getTableAlias(false, false) . ".$field $dir";
-	}	
+    private function getSortSQL($field, $dir = 'ASC') {
+        return $this->Owner->getTableAlias(false, false) . ".$field $dir";
+    }
 
 }
